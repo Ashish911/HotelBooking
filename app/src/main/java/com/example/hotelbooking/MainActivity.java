a0eb16e6c566eff7aa4e7c05e8e940fc17655f2d
Ashish911 <ashishdongol32@gmail.com>
2019-11-20 20:36:41 +0545

    Complete

--- app/src/main/java/com/example/hotelbooking/MainActivity.java
+++ app/src/main/java/com/example/hotelbooking/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -11,7 +13,10 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -81,8 +86,39 @@ public class MainActivity extends AppCompatActivity {
         btnCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                if (TextUtils.isEmpty(tvCheckIn.getText())){
+                    tvCheckIn.setError("Enter CheckIn Date");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(tvCheckOut.getText())){
+                    tvCheckOut.setError("Enter CheckOut Date");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(etAdult.getText())){
+                    etAdult.setError("Enter No of Adults");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(etChildren.getText())){
+                    etChildren.setError("Enter No of Children");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(etRooms.getText())){
+                    etRooms.setError("Enter No of Rooms");
+                    return;
+                }
+
                 String RoomType = spinRoom.getSelectedItem().toString();
                 String Place =spinLocation.getSelectedItem().toString();
+
+                if (Place == "Select Location"){
+                    Toast.makeText(MainActivity.this, "Please select your Location", Toast.LENGTH_SHORT).show();
+                }
+
                 String CheckIn = tvCheckIn.getText().toString();
                 String CheckOut = tvCheckOut.getText().toString();
                 int TotalRooms = Integer.parseInt(etRooms.getText().toString());
@@ -92,30 +128,44 @@ public class MainActivity extends AppCompatActivity {
                 int Vat;
                 int ServiceCharge;
 
-                if (RoomType == "Delux"){
-                    RoomValue = 2000;
-                }
-                else if (RoomType == "AC"){
-                    RoomValue = 3000;
-                }
-                else if (RoomType == "Platinum"){
-                    RoomValue = 4000;
-                }
+                SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+                try {
+                    Date date1 = sdf.parse(CheckIn);
+                    Date date2 = sdf.parse(CheckOut);
+                    long diff = date2.getTime() - date1.getTime();
+                    long diffDays = diff / (24 * 60 * 60 * 1000);
+                    int days = (int) diffDays;
 
-                Price = RoomValue * TotalRooms;
-                Vat = (Price * 13)/100;
-                ServiceCharge = (Price * 10)/100;
-                TotalPrice = Price + Vat + ServiceCharge;
+                    if (RoomType == "Delux"){
+                        RoomValue = 2000;
+                    }
+                    else if (RoomType == "AC"){
+                        RoomValue = 3000;
+                    }
+                    else if (RoomType == "Platinum"){
+                        RoomValue = 4000;
+                    }
+                    else
+                    {
+                        Toast.makeText(MainActivity.this, "Please Select Room Type", Toast.LENGTH_SHORT).show();
+                    }
 
-                tvLocation.setText("Location : ".concat(Place));
-                tvRoomType.setText("Room Type : ".concat(RoomType));
-                tvOutputCheckIn.setText("CheckIn : ".concat(CheckIn));
-                tvOutputCheckOut.setText("CheckOut : ".concat(CheckOut));
-                tvTotalRoom.setText("Total Rooms: ".concat(Integer.toString(TotalRooms)));
-                tvServiceC.setText("Service Charge: ".concat(Integer.toString(ServiceCharge)));
-                tvVat.setText("Vat: ".concat(Integer.toString(Vat)));
-                tvTotal.setText("Total : " .concat(Integer.toString(TotalPrice)));
+                    Price = RoomValue * TotalRooms * days;
+                    Vat = (Price * 13)/100;
+                    ServiceCharge = (Price * 10)/100;
+                    TotalPrice = Price + Vat + ServiceCharge;
 
+                    tvLocation.setText("Location : ".concat(Place));
+                    tvRoomType.setText("Room Type : ".concat(RoomType));
+                    tvOutputCheckIn.setText("CheckIn : ".concat(CheckIn));
+                    tvOutputCheckOut.setText("CheckOut : ".concat(CheckOut));
+                    tvTotalRoom.setText("Total Rooms: ".concat(Integer.toString(TotalRooms)));
+                    tvServiceC.setText("Service Charge: ".concat(Integer.toString(ServiceCharge)));
+                    tvVat.setText("Vat: ".concat(Integer.toString(Vat)));
+                    tvTotal.setText("Total : " .concat(Integer.toString(TotalPrice)));
+                } catch (ParseException e){
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -131,6 +181,7 @@ public class MainActivity extends AppCompatActivity {
                 this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
                 String date = month + "/" + dayOfMonth + "/" + year;
                 if (d1 == true){
                     tvCheckIn.setText(date);
